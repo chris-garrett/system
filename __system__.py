04d3915a695a2b3ac5a5862b843ba682dc2b5199
Chris Garrett <chris@nesteggs.ca>
2023-12-05 19:02:41 -0700

    feat: add system helper for apt/snap installs

---
+++ __system__.py
@@ -0,0 +1,55 @@
+import os
+from __task__ import TaskContext
+
+
+def snap_install(ctx: TaskContext, app: str, classic=False, edge=False):
+    """
+    Function to install an application using snap package manager.
+
+    Args:
+        ctx (TaskContext): Context object that provides system details and logging.
+        app (str): The name of the application to be installed.
+        classic (bool, optional): Install using --classic flag. Defaults to False.
+        edge (bool, optional): Install using --edge flag. Defaults to False.
+
+    Raises:
+        NotImplementedError: If the system distro is not debian.
+    """
+
+    extra_repo = ""
+    if classic:
+        extra_repo = "--classic"
+    if edge:
+        extra_repo = "--edge"
+
+    if ctx.system.platform == "linux":
+        if not os.path.exists(f"/snap/bin/{app}"):
+            ctx.log.info(f"installing {app}")
+            ctx.exec(f"sudo snap install {extra_repo}{app}")
+        else:
+            ctx.log.info(f"{app} already installed")
+    else:
+        raise NotImplementedError(f"{app} not implemented on platform: {ctx.system.platform}:{ctx.system.distro}")
+
+
+def apt_install(ctx: TaskContext, app: str, file_test: str):
+    """
+    Function to install an application using apt package manager.
+
+    Args:
+        ctx (TaskContext): Context object that provides system details and logging.
+        app (str): The name of the application to be installed.
+        file_test (str): The path to check if the application is already installed.
+
+    Raises:
+        NotImplementedError: If the system distro is not debian.
+    """
+
+    if ctx.system.distro == "debian":
+        if not os.path.exists(file_test):
+            ctx.log.info(f"installing {app}")
+            ctx.exec(f"sudo apt install -y {app}")
+        else:
+            ctx.log.info(f"{app} already installed")
+    else:
+        raise NotImplementedError(f"{app} not implemented on platform: {ctx.system.platform}:{ctx.system.distro}")
