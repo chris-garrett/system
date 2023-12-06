bc3623dce46b46c0fcbaa05d63478dea4fd434a0
Chris Garrett <chris@nesteggs.ca>
2023-12-05 19:37:39 -0700

    feat: browsers, brave, edge, chrome

--- browsers/__task__.py
+++ browsers/__task__.py
@@ -1,27 +1,29 @@
 import os
 from __task__ import TaskContext, TaskBuilder
+from __system__ import snap_install, apt_install
+
+module_name = "browsers"
 
 
-def _install(ctx: TaskContext, app):
-    if ctx.system.platform == "linux":
-        if not os.path.exists(f"/snap/bin/{app}"):
-            ctx.log.info(f"installing {app}")
-            ctx.exec(f"sudo snap install {app}")
+def _edge(ctx: TaskContext):
+    if ctx.system.distro == "debian":
+        if not os.path.exists("/usr/bin/microsoft-edge-stable"):
+            ctx.exec("curl -o /tmp/msedge.deb -L -C - 'https://go.microsoft.com/fwlink?linkid=2149051&brand=M102'")
+            ctx.exec("sudo dpkg -i /tmp/msedge.deb")
+            ctx.exec("sudo apt install -f")
         else:
-            ctx.log.info(f"{app} already installed")
+            ctx.log.info("edge already installed")
     else:
-        raise NotImplementedError(f"{app} not implemented on platform: {ctx.system.platform}:{ctx.system.distro}")
-
-
-def _brave(ctx: TaskContext):
-    _install(ctx, "brave")
-
-
-def _all(ctx: TaskContext):
-    _brave(ctx)
+        raise NotImplementedError(f"edge not implemented on platform: {ctx.system.platform}:{ctx.system.distro}")
 
 
 def configure(builder: TaskBuilder):
-    module_name = "browsers"
-    builder.add_task(module_name, "browsers", _all)
-    builder.add_task(module_name, "browsers:brave", _brave)
+    builder.add_task(
+        module_name,
+        f"{module_name}:all",
+        lambda ctx: None,
+        deps=[f"{module_name}:brave", f"{module_name}:chrome", f"{module_name}:edge"],
+    )
+    builder.add_task(module_name, f"{module_name}:brave", lambda ctx: snap_install(ctx, "brave"))
+    builder.add_task(module_name, f"{module_name}:chrome", lambda ctx: snap_install(ctx, "chromium"))
+    builder.add_task(module_name, f"{module_name}:edge", _edge)
