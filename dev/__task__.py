60be36f72620b3525b282d48be555cb1d8cb26e0
Chris Garrett <chris@nesteggs.ca>
2023-12-05 20:51:03 -0700

    chore: move gitkraken, dotnet to dev

--- dev/__task__.py
+++ dev/__task__.py
@@ -1,3 +1,4 @@
+import os
 from __task__ import TaskBuilder, TaskContext
 from __system__ import snap_install, apt_install
 
@@ -14,9 +15,48 @@ def _git_config(ctx: TaskContext):
         ctx.exec("git config --global push.default current")
 
 
+def _dotnet_installer(ctx: TaskContext, version: str):
+    if ctx.system.platform == "linux":
+        print("dist 1")
+        dotnet = os.path.expanduser("~/.dotnet/dotnet")
+        installer_file = "/tmp/dotnet-install.sh"
+
+        if not os.path.exists(installer_file):
+            print("dist 2")
+            ctx.exec(f"curl -L -o {installer_file} https://dot.net/v1/dotnet-install.sh")
+
+        ret = ctx.exec(f"{dotnet} --list-sdks", quiet=True)
+        if ret.returncode != 0:
+            raise Exception(f"Failed to list dotnet sdks: {ret.stderr}")
+
+        found = len([x for x in ret.stdout.splitlines() if x.startswith(version)])
+        if not found:
+            ctx.exec(f"/bin/bash {installer_file} -c {version}")
+
+
+def _noop(ctx: TaskContext):
+    ctx.log.info("installing all .net versions")
+
+
 def configure(builder: TaskBuilder):
     builder.add_task(module_name, f"{module_name}:meld", lambda ctx: apt_install(ctx, "meld", "/usr/bin/meld"))
     builder.add_task(module_name, f"{module_name}:postman", lambda ctx: snap_install(ctx, "postman"))
     builder.add_task(module_name, f"{module_name}:dbeaver", lambda ctx: snap_install(ctx, "dbeaver-ce"))
     builder.add_task(module_name, f"{module_name}:git:config", _git_config)
     builder.add_task(module_name, f"{module_name}:git:lfs", lambda ctx: apt_install(ctx, "git-lfs", "/usr/bin/git-lfs"))
+    builder.add_task(module_name, f"{module_name}:gitkraken", lambda ctx: snap_install(ctx, "gitkraken", classic=True))
+    builder.add_task(
+        module_name,
+        f"{module_name}:dotnet:all",
+        _noop,
+        deps=[f"{module_name}:dotnet:6", f"{module_name}:dotnet:7", f"{module_name}:dotnet:8"],
+    )
+    builder.add_task(
+        module_name, f"{module_name}:dotnet:6", lambda ctx: _dotnet_installer(ctx, "6.0"), deps=["utils:curl"]
+    )
+    builder.add_task(
+        module_name, f"{module_name}:dotnet:7", lambda ctx: _dotnet_installer(ctx, "7.0"), deps=["utils:curl"]
+    )
+    builder.add_task(
+        module_name, f"{module_name}:dotnet:8", lambda ctx: _dotnet_installer(ctx, "8.0"), deps=["utils:curl"]
+    )
