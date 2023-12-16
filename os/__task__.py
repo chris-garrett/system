3a97c112c4678e873560a2159a9ec1f7a8be5d9c
Chris Garrett <chris@nesteggs.ca>
2023-12-16 16:34:27 -0700

    feat: upgrade snap when doing os upgrade

--- os/__task__.py
+++ os/__task__.py
@@ -7,8 +7,13 @@ def _print_info(ctx: TaskContext):
     print(f"Arch:     {ctx.system.arch}")
 
 
+def _upgrade(ctx: TaskContext):
+    ctx.exec("sudo apt upgrade -y")
+    ctx.exec("sudo snap refresh")
+
+
 def configure(builder: TaskBuilder):
     module_name = "os"
     builder.add_task(module_name, "os:update", lambda ctx: ctx.exec("sudo apt update"))
-    builder.add_task(module_name, "os:upgrade", lambda ctx: ctx.exec("sudo apt upgrade -y"), deps=["os:update"])
+    builder.add_task(module_name, "os:upgrade", _upgrade, deps=["os:update"])
     builder.add_task(module_name, "os:info", _print_info)
