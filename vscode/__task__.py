f57756171d8dd47fcb0e1833672cde335e6fd1cc
Chris Garrett <chris@nesteggs.ca>
2023-12-16 16:38:25 -0700

    feat: switch from vscode to vscodium

--- vscode/__task__.py
+++ vscode/__task__.py
@@ -1,39 +1,39 @@
-import os
 from __task__ import TaskContext, TaskBuilder
-
-
-def _setup(ctx: TaskContext):
-    if ctx.system.distro == "debian":
-        if not os.path.exists("/snap/bin/code"):
-            ctx.log.info("installing")
-            ctx.exec("sudo snap install --classic code")
-        else:
-            ctx.log.info("already installed")
-    else:
-        raise NotImplementedError(f"Not implemented on platform: {ctx.system.platform}:{ctx.system.distro}")
+from __system__ import get_github_download_url, deb_install
 
 
 def _python(ctx: TaskContext):
-    ctx.exec("code --install-extension ms-python.python")
-    ctx.exec("code --install-extension ms-toolsai.jupyter")
-    ctx.exec("code --install-extension ms-python.black-formatter")
-    ctx.exec("code --install-extension ms-python.flake8")
+    ctx.exec("codium --install-extension ms-python.python")
+    ctx.exec("codium --install-extension ms-toolsai.jupyter")
+    ctx.exec("codium --install-extension ms-python.black-formatter")
+    ctx.exec("codium --install-extension ms-python.flake8")
 
 
 def _aiml(ctx: TaskContext):
-    ctx.exec("code --install-extension Continue.continue")
-    ctx.exec("code --install-extension GitHub.copilot")
-    ctx.exec("code --install-extension GitHub.copilot-chat")
-    ctx.exec("code --install-extension ms-semantic-kernel.semantic-kernel")
+    ctx.exec("codium --install-extension Continue.continue")
+    ctx.exec("codium --install-extension GitHub.copilot")
+    ctx.exec("codium --install-extension GitHub.copilot-chat")
+    ctx.exec("codium --install-extension ms-semantic-kernel.semantic-kernel")
 
 
 def _dotnet(ctx: TaskContext):
-    ctx.exec("code --install-extension ms-dotnettools.csdevkit")
+    ctx.exec("codium --install-extension ms-dotnettools.csdevkit")
+
+
+def _install_codium(ctx: TaskContext):
+    # TODO: add alias code -> codium
+    if ctx.system.arch == "x86_64" and ctx.system.distro == "debian":
+        deb_url = get_github_download_url("VSCodium", "vscodium", r"amd64.deb$")
+        deb_install(ctx, "codium", "/usr/bin/codium", deb_url)
+    else:
+        raise NotImplementedError(
+            f"Not implemented on platform: {ctx.system.platform}:{ctx.system.distro}:{ctx.system.arch}"
+        )
 
 
 def configure(builder: TaskBuilder):
     module_name = "vscode"
-    builder.add_task(module_name, "vscode:code", _setup)
+    builder.add_task(module_name, "vscode:code", _install_codium)
     builder.add_task(module_name, "vscode:python", _python)
     builder.add_task(module_name, "vscode:aiml", _aiml)
     builder.add_task(module_name, "vscode:dotnet", _dotnet)
