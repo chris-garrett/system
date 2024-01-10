70a91611b4fbbfcacaccb34aa732b380ae236c0e
Chris Garrett <chris@nesteggs.ca>
2024-01-09 21:52:32 -0700

    vscode: revert to ms code out of apt. snap and codium are not useful yet.

--- vscode/__task__.py
+++ vscode/__task__.py
@@ -1,3 +1,4 @@
+import os
 from __task__ import TaskContext, TaskBuilder
 from __system__ import get_github_download_url, deb_install
 
@@ -21,6 +22,7 @@ def _dotnet(ctx: TaskContext):
 
 
 def _install_codium(ctx: TaskContext):
+    # TODO: codium doesnt have any of the plugins i care about. figure out how to get them
     # TODO: add alias code -> codium
     if ctx.system.arch == "x86_64" and "debian" in ctx.system.distro:
         deb_url = get_github_download_url(ctx, "VSCodium", "vscodium", r"amd64.deb$")
@@ -31,9 +33,27 @@ def _install_codium(ctx: TaskContext):
         )
 
 
+def _install_ms_code(ctx: TaskContext):
+    if ctx.system.arch == "x86_64" and "debian" in ctx.system.distro:
+        if not os.path.exists("/usr/bin/code"):
+            ctx.log.info("installing")
+            ctx.exec("sudo apt update")
+            ctx.exec("sudo apt install software-properties-common apt-transport-https wget")
+            ctx.exec("wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -")
+            ctx.exec(
+                'sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"'
+            )
+            ctx.exec("sudo apt install -y code")
+        else:
+            ctx.log.info("already installed")
+    else:
+        raise NotImplementedError(f"Not implemented on platform: {ctx.system.platform}:{ctx.system.distro}")
+
+
 def configure(builder: TaskBuilder):
     module_name = "vscode"
-    builder.add_task(module_name, "vscode:code", _install_codium)
+    builder.add_task(module_name, "vscode:code", _install_ms_code)
+    builder.add_task(module_name, "vscode:codium", _install_codium)
     builder.add_task(module_name, "vscode:python", _python)
     builder.add_task(module_name, "vscode:aiml", _aiml)
     builder.add_task(module_name, "vscode:dotnet", _dotnet)
