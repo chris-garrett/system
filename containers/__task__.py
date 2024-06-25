92c9898761bb855313e6085193c7cab1f4568ee8
Chris Garrett <chris@nesteggs.ca>
2024-06-25 02:37:58 +0000

    containers: add start of nvidia docker install

--- containers/__task__.py
+++ containers/__task__.py
@@ -1,11 +1,9 @@
 import os
 import re
+
+from __system__ import (get_github_release, usr_binary_install,
+                        usr_binary_install_github)
 from __task__ import TaskBuilder, TaskContext
-from __system__ import (
-    get_github_release,
-    usr_binary_install,
-    usr_binary_install_github,
-)
 
 module_name = "containers"
 
@@ -38,6 +36,21 @@ def _setup_docker(ctx: TaskContext):
 
     else:
         raise NotImplementedError("docker not implemented")
+
+
+def _nvidia(ctd: TaskContext):
+    """
+    TODO: make this work
+    curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+    sudo apt-get update
+    sudo apt-get install -y nvidia-container-toolkit
+    sudo nvidia-ctk runtime configure --runtime=docker
+    sudo systemctl restart docker
+    """
+    pass
 
 
 def _kubectl(ctx: TaskContext):
