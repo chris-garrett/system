90b225f030d52a0f50b3c2e92ddf3b2fd70464c8
Chris Garrett <chris@nesteggs.ca>
2024-10-01 13:23:20 -0600

    vi: add mouse toggle

--- vim/nvim/lua/vim-options.lua
+++ vim/nvim/lua/vim-options.lua
@@ -19,3 +19,19 @@ vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
 
 vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
+
+-- Function to toggle mouse setting between `mouse=` and `mouse=a`
+local function toggle_mouse()
+  if vim.o.mouse == 'a' then
+    --vim.o.mouse = ''
+    vim.cmd("set mouse=")
+    print("Mouse disabled")
+  else
+    --vim.o.mouse = 'a'
+    vim.cmd("set mouse=a")
+    print("Mouse enabled")
+  end
+end
+
+-- Map <leader>m to toggle the mouse setting
+vim.keymap.set('n', '<leader>m', toggle_mouse)
